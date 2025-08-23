a770bd680182b5d18a081c902546afb483131447
farisalfrz <faris.alfarizi109@gmail.com>
2025-08-23 14:08:29 +0700

    edit rss url

--- backend/main.py
+++ backend/main.py
@@ -52,7 +52,7 @@ def get_latest_hoaxes():
 
     print("Mengambil data hoaks baru dari RSS Feed...")
     try:
-        RSS_URL = "https://cekfakta.tempo.co/rss"
+        RSS_URL = "https://cekfakta.tempo.co/"
         
         headers = {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
